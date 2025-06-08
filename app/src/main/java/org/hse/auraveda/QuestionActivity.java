181fdb74c05a07ae8ad26d4743cd056eb87bb189
bbelochkka <bbelochkka@LAPTOP-RJ17VHPO>
2025-06-08 21:24:27 +0500

    first commit

---
+++ app/src/main/java/org/hse/auraveda/QuestionActivity.java
@@ -0,0 +1,30 @@
+package org.hse.auraveda;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_question1);
+
+        // Используем корневой ConstraintLayout из XML
+        View rootView = findViewById(R.id.rootConstraintLayout);
+        if (rootView != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(rootView, (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+        }
+    }
+}
