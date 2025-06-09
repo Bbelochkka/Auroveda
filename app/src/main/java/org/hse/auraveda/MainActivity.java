a3a9b9394ac227d5d11d326d909b6be537b22331
bbelochkka <bbelochkka@LAPTOP-RJ17VHPO>
2025-06-09 22:38:52 +0500

    first two screens

--- app/src/main/java/org/hse/auraveda/MainActivity.java
+++ app/src/main/java/org/hse/auraveda/MainActivity.java
@@ -1,6 +1,9 @@
 package org.hse.auraveda;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,20 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Находим кнопку по ID
+        Button buttonCard = findViewById(R.id.buttonCard);
+
+        // Устанавливаем обработчик нажатия
+        buttonCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTickets();
+            }
+        });
+    }
+    private void showTickets() {
+        Intent intent = new Intent(this, TicketsActivity.class);
+        startActivity(intent);
     }
 }
