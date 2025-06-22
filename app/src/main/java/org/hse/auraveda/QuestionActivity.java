6d9c8170ea30e09c04ac3230e5a8bd15497200cd
AoAlenaA <aoanuchina@edu.hse.ru>
2025-06-22 17:37:57 +0500

    feature: fixed questions design

--- app/src/main/java/org/hse/auraveda/QuestionActivity.java
+++ app/src/main/java/org/hse/auraveda/QuestionActivity.java
@@ -1,5 +1,6 @@
 package org.hse.auraveda;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -29,7 +31,6 @@ public class QuestionActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_question);
 
-        // Используем корневой ConstraintLayout из XML
         View rootView = findViewById(R.id.rootConstraintLayout);
         if (rootView != null) {
             ViewCompat.setOnApplyWindowInsetsListener(rootView, (v, insets) -> {
@@ -45,8 +46,7 @@ public class QuestionActivity extends AppCompatActivity {
             return;
         }
 
-        // Загружаем вопросы для данного билета
-        dbHelper = new DatabaseHelper(this); // Убрали тип DatabaseHelper
+        dbHelper = new DatabaseHelper(this);
         questions = dbHelper.getQuestionsByCardId(cardId);
 
         if (questions.isEmpty()) {
@@ -55,7 +55,6 @@ public class QuestionActivity extends AppCompatActivity {
             return;
         }
 
-        // Настройка UI
         setupQuestionIndicators();
         setupNavigationButtons();
         setupAnswerButtonsListeners();
@@ -67,7 +66,7 @@ public class QuestionActivity extends AppCompatActivity {
         indicatorLayout.removeAllViews();
 
         for (int i = 0; i < questions.size(); i++) {
-            Button button = new Button(this);
+            AppCompatButton button = new AppCompatButton(this);
             button.setText(String.valueOf(i + 1));
             button.setLayoutParams(new LinearLayout.LayoutParams(
                     0,
@@ -100,38 +99,56 @@ public class QuestionActivity extends AppCompatActivity {
             }
         });
     }
+
     private void setupAnswerButtonsListeners() {
         LinearLayout answerLayout = findViewById(R.id.answerOptions);
         for (int i = 0; i < answerLayout.getChildCount(); i++) {
-            Button button = (Button) answerLayout.getChildAt(i);
-            button.setOnClickListener(v -> checkAnswer(button));
+            View view = answerLayout.getChildAt(i);
+            if (view instanceof AppCompatButton) {
+                AppCompatButton button = (AppCompatButton) view;
+                button.setOnClickListener(v -> checkAnswer(button));
+            }
         }
     }
 
-    private void checkAnswer(Button selectedButton) {
+    private void checkAnswer(AppCompatButton selectedButton) {
         Question currentQuestion = questions.get(currentQuestionIndex);
         String selectedAnswer = selectedButton.getText().toString();
 
-        Log.d("AnswerCheck", "Selected answer: " + selectedAnswer);
-        Log.d("AnswerCheck", "Correct answer: " + currentQuestion.getCorrectAnswer());
-        Log.d("AnswerCheck", "Question ID: " + currentQuestion.getId());
-
         if (selectedAnswer.equals(currentQuestion.getCorrectAnswer())) {
-            selectedButton.setBackgroundColor(ContextCompat.getColor(this, R.color.correct_answer));
-            Log.d("AnswerCheck", "Answer is correct");
+            // Правильный ответ - зеленый
+            selectedButton.setBackgroundTintList(
+                    ColorStateList.valueOf(ContextCompat.getColor(this, R.color.correct_answer))
+            );
         } else {
-            selectedButton.setBackgroundColor(ContextCompat.getColor(this, R.color.wrong_answer));
-            Log.d("AnswerCheck", "Answer is wrong");
+            // Неправильный ответ - красный
+            selectedButton.setBackgroundTintList(
+                    ColorStateList.valueOf(ContextCompat.getColor(this, R.color.wrong_answer))
+            );
 
-            // Проверяем и добавляем ошибку в базу
             if (!dbHelper.isMistakeExists(currentQuestion.getId())) {
-                Log.d("MistakeDB", "Adding mistake for question: " + currentQuestion.getId());
                 dbHelper.addMistake(currentQuestion.getId());
-            } else {
-                Log.d("MistakeDB", "Mistake already exists for question: " + currentQuestion.getId());
             }
         }
         disableAnswerButtons();
+
+        // Показать правильный ответ
+        highlightCorrectAnswer(currentQuestion);
+    }
+
+    private void highlightCorrectAnswer(Question question) {
+        LinearLayout answerLayout = findViewById(R.id.answerOptions);
+        for (int i = 0; i < answerLayout.getChildCount(); i++) {
+            View view = answerLayout.getChildAt(i);
+            if (view instanceof AppCompatButton) {
+                AppCompatButton button = (AppCompatButton) view;
+                if (button.getText().toString().equals(question.getCorrectAnswer())) {
+                    button.setBackgroundTintList(
+                            ColorStateList.valueOf(ContextCompat.getColor(this, R.color.correct_answer))
+                    );
+                }
+            }
+        }
     }
 
     private void disableAnswerButtons() {
@@ -140,12 +157,19 @@ public class QuestionActivity extends AppCompatActivity {
             answerLayout.getChildAt(i).setEnabled(false);
         }
     }
+
     private void resetAnswerButtons() {
         LinearLayout answerLayout = findViewById(R.id.answerOptions);
         for (int i = 0; i < answerLayout.getChildCount(); i++) {
-            Button button = (Button) answerLayout.getChildAt(i);
-            button.setEnabled(true);
-            button.setBackgroundColor(ContextCompat.getColor(this, R.color.default_button_color));
+            View view = answerLayout.getChildAt(i);
+            if (view instanceof AppCompatButton) {
+                AppCompatButton button = (AppCompatButton) view;
+                button.setEnabled(true);
+                // Возвращаем исходный цвет кнопки
+                button.setBackgroundTintList(
+                        ColorStateList.valueOf(ContextCompat.getColor(this, R.color.default_button_color))
+                );
+            }
         }
     }
 
@@ -160,22 +184,37 @@ public class QuestionActivity extends AppCompatActivity {
         questionText.setText(question.getName());
 
         LinearLayout answerLayout = findViewById(R.id.answerOptions);
-        ((Button) answerLayout.getChildAt(0)).setText(question.getOption1());
-        ((Button) answerLayout.getChildAt(1)).setText(question.getOption2());
-        ((Button) answerLayout.getChildAt(2)).setText(question.getCorrectAnswer());
+        for (int i = 0; i < answerLayout.getChildCount(); i++) {
+            View view = answerLayout.getChildAt(i);
+            if (view instanceof AppCompatButton) {
+                AppCompatButton button = (AppCompatButton) view;
+                switch (i) {
+                    case 0: button.setText(question.getOption1()); break;
+                    case 1: button.setText(question.getOption2()); break;
+                    case 2: button.setText(question.getCorrectAnswer()); break;
+                }
+            }
+        }
 
-        resetAnswerButtons(); // Сбрасываем состояние кнопок
+        resetAnswerButtons();
         updateQuestionIndicators(index);
     }
 
     private void updateQuestionIndicators(int currentIndex) {
         LinearLayout indicatorLayout = findViewById(R.id.questionIndicator);
         for (int i = 0; i < indicatorLayout.getChildCount(); i++) {
-            Button button = (Button) indicatorLayout.getChildAt(i);
-            if (i == currentIndex) {
-                button.setBackgroundColor(ContextCompat.getColor(this, R.color.current_question));
-            } else {
-                button.setBackgroundColor(ContextCompat.getColor(this, R.color.default_question_indicator));
+            View view = indicatorLayout.getChildAt(i);
+            if (view instanceof AppCompatButton) {
+                AppCompatButton button = (AppCompatButton) view;
+                if (i == currentIndex) {
+                    button.setBackgroundTintList(
+                            ColorStateList.valueOf(ContextCompat.getColor(this, R.color.current_question))
+                    );
+                } else {
+                    button.setBackgroundTintList(
+                            ColorStateList.valueOf(ContextCompat.getColor(this, R.color.default_question_indicator))
+                    );
+                }
             }
         }
     }
@@ -183,5 +222,4 @@ public class QuestionActivity extends AppCompatActivity {
     private int getCardId() {
         return getIntent().getIntExtra("CARD_ID", 1);
     }
-
 }
